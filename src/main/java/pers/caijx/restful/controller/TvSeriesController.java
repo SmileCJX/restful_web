87c8dfcb0ac40a1d5cc39a0a52fa0743269c0767
SmileCJX <1092925421@qq.com>
2018-07-09 23:30:31 +0800

    电视列表返回

--- src/main/java/pers/caijx/restful/controller/TvSeriesController.java
+++ src/main/java/pers/caijx/restful/controller/TvSeriesController.java
@@ -3,9 +3,9 @@ package pers.caijx.restful.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pers.caijx.restful.dto.TvseriesDto;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Administrator on 2018/7/9/009.
@@ -15,9 +15,11 @@ import java.util.Map;
 public class TvSeriesController {
 
     @GetMapping
-    public Map<String,Object> sayHello() {
-        Map<String,Object> result = new HashMap<>();
-        result.put("message","hello,world");
-        return result;
+    public List<TvseriesDto> getAll() {
+        List<TvseriesDto> list = new ArrayList<>();
+        Calendar cal = Calendar.getInstance();
+        cal.set(2016,Calendar.OCTOBER,2,0,0);
+        list.add(new TvseriesDto(1,"WestLand",1,cal.getTime()));
+        return list;
     }
 }
